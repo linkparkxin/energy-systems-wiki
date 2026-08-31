26b0154752416fec6380058b00568c98b21abe0e
Mingxin Liu <161667043+linkparkxin@users.noreply.github.com>
2026-08-31 16:33:14 +0800

    fix: preserve default not-found layout

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
-import { h } from 'vue'
 import WalineComment from './WalineComment.vue'
 import '@waline/client/style'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'not-found': () => h('div')
-    })
-  },
   enhanceApp({ app }) {
     app.component('WalineComment', WalineComment)
   }
