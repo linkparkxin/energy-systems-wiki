375f4bc7d1401de7d68c91043082deb8ea71324f
Mingxin Liu <161667043+linkparkxin@users.noreply.github.com>
2026-09-01 09:10:45 +0800

    feat: add Supabase realtime community Q&A

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
+import SupabaseDiscussion from './SupabaseDiscussion.vue'
 import WalineComment from './WalineComment.vue'
 import '@waline/client/style'
 import './custom.css'
@@ -6,6 +7,7 @@ import './custom.css'
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
+    app.component('SupabaseDiscussion', SupabaseDiscussion)
     app.component('WalineComment', WalineComment)
   }
 }
